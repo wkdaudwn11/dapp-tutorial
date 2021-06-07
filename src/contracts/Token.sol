bae0ac7b18967db36ce2ccd8ce0a5d0c20a55e86
JangMyungJoo <wkdaudwn11@naver.com>
2021-06-07 15:42:27 +0900

    feat. Token.sol 작성

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,18 +4,29 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+  //add minter variable 
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
+  constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dBank) public returns (bool) {
+    require(msg.sender == minter, 'Error, only owner can change pass minter role.');
+    minter = dBank;
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender == minter, 'Error, msg.sender does not have minter role.');
+
 		_mint(account, amount);
 	}
 }
